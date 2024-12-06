4405211a743db09253e511831a083918188b2d61
zkak345 <112196672+zkak345@users.noreply.github.com>
2024-12-06 16:02:05 -0600

    final unit test

--- unittests/cam_test.py
+++ unittests/cam_test.py
@@ -1,84 +1,135 @@
 import unittest
 import sys
 sys.path.append("../MathWiz-ATWM")
+#create objects
 from unittest.mock import MagicMock
 import GameData as state
 from Cam import cam 
 
 class TestCam(unittest.TestCase):
-    #Initialization test to make sure all is good
-    def test_initialization(self):
-        state.screensize = [800, 600]
-        #get class
+    #camera setup
+    def test_setup(self):
+        #set up screen, i made it 500x500 for ease
+        state.screensize = [500, 500]
+        #actually set up cam
         camera = cam()
 
         #simple assertion
         #get center camera
-        self.assertEqual(camera.focus, [400, 300])  
+        #focuse on half
+        self.assertEqual(camera.focus, [250, 250])  
+        #make sure its center
         self.assertEqual(camera.pos, (0, 0))
-        self.assertEqual(camera.lastpos, (0, 0))
         self.assertEqual(camera.depth, 0)
-        self.assertIsNone(camera.focusobj)
 
-    def test_update_no_focus(self):
+    def test_no_focus(self):
+        #set a small screen
         state.screensize = [800, 600]
-        #test play
+        #put it in play gamemode instead of edit
         state.gamemode = "play" 
         #no objects 
         state.objects = []
         camera = cam()
-        
+        #update settings
         camera.update()
 
         #test if camera position changes
-        self.assertEqual(camera.pos, (0, 0))
+        self.assertEqual(camera.pos, (0, 0))   #this is just to make sure its position is in center
         self.assertEqual(camera.lastpos, (0, 0))
 
-    def test_focus_objects(self):
-        state.screensize = [800, 600]
+    def test_focus_objects_regular(self):
+        #screen size random nums to test
+        state.screensize = [600, 600]
         state.gamemode = "play"
         state.objects = []
 
+        #create mock object like earlier
         mock_focus_object = MagicMock()
-        mock_focus_object.pos = [1000, 800]
-        mock_focus_object.parallax = 1  # Default parallax
-
+        #add a position to focus, can be any value I just chose 800,400
+        mock_focus_object.pos = [800, 400]
+        #set up cam
         camera = cam()
         camera.focusobj = mock_focus_object
+        #update
         camera.update()
 
-        # The camera should center on the focus object
-        self.assertEqual(camera.focus, [1000, 800])
-        self.assertEqual(camera.pos, (600, 500))  # Adjusted for screen size
+        #okay, test to see if there is focus on the object
+        self.assertEqual(camera.focus, [800, 400])  #should be same nums as object position
+        #calculate cam position
+        #focus[0] - screensize[0]/2 = 800 - 600/2 = 500
+        #focus[1] - screensize[1]/2 = 400 - 600/2 = 100
+        #make sure the nums match
+        self.assertEqual(camera.pos, (500, 100))  
+        #success
 
-
-
-    def test_update_with_parallax(self):
-        state.screensize = [800, 600]
+    #Focus on a negative position, handle negative numbers
+    def test_focus_objects_negative(self):
+        #same nums as above
+        state.screensize = [600, 600]
         state.gamemode = "play"
+        state.objects = []
 
+        #create mock object like earlier
         mock_focus_object = MagicMock()
-        mock_focus_object.pos = [1000, 800]
-        mock_focus_object.parallax = 2  # Parallax value
-
+        #add a position to focus, this time 200 for second, so we can get a negative num, explained in a sec
+        mock_focus_object.pos = [800, 200]
+       
+        #set cam
         camera = cam()
         camera.focusobj = mock_focus_object
         camera.update()
 
-        # Depth and position should reflect parallax
-        self.assertEqual(camera.depth, 1)
-        self.assertEqual(camera.pos, (600, 500))  # Adjusted for parallax
-    
-    def test_update_in_edit_mode(self):
-        state.screensize = [800, 600]
+        #obj position
+        self.assertEqual(camera.focus, [800, 200])
+        #as in test above
+        # 800 - 600/2 = 500
+        # 200 - 600/2 = -100 
+        #test to see if it pans -100
+        self.assertEqual(camera.pos, (500, -100))
+        #success
+
+    #testing edit mode
+    def test_edit(self):
+        #set screen size
+        state.screensize = [600, 600]
+        #set gamemode to edit
         state.gamemode = "edit"
         state.objects = []
 
         camera = cam()
-        camera.focus = [100, 100]
+        camera.focus = [400, 500]
         camera.update()
 
-        # Camera should freely move in edit mode
-        self.assertEqual(camera.pos, (-300, -200))  # Centering focus
+        #same equation 
+        #400 - 600/2 = 100
+        # 500 - 600/2 = 200
+        #testing
+        self.assertEqual(camera.pos, (100, 200))  
+        #success
+
+    #test object parallax
+    def test_with_parallax(self):
+        state.screensize = [600, 600]
+        state.gamemode = "play"
+
+        #mock object
+        mock_focus_object = MagicMock()
+        mock_focus_object.pos = [400, 500]
+        #set up parallax to tes depth
+        mock_focus_object.parallax = 4 
+
+        camera = cam()
+        camera.focusobj = mock_focus_object
+        camera.update()
+
+        #first depth
+        # 4 - 1 = 3
+        #test depth of 3
+        self.assertEqual(camera.depth, 3)
+        #success
+        #test cam position same equation
+        self.assertEqual(camera.pos, (100, 200))
+        #success 
+    
 
 unittest.main()
