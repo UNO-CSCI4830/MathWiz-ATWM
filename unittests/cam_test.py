56e077725b254d5037ea9f005b1475bae9e68bd1
zkak345 <112196672+zkak345@users.noreply.github.com>
2024-12-06 01:27:17 -0600

    more unittests

--- unittests/cam_test.py
+++ unittests/cam_test.py
@@ -1,4 +1,6 @@
 import unittest
+import sys
+sys.path.append("../MathWiz-ATWM")
 from unittest.mock import MagicMock
 import GameData as state
 from Cam import cam 
@@ -48,6 +50,35 @@ class TestCam(unittest.TestCase):
         # The camera should center on the focus object
         self.assertEqual(camera.focus, [1000, 800])
         self.assertEqual(camera.pos, (600, 500))  # Adjusted for screen size
+
+
+
+    def test_update_with_parallax(self):
+        state.screensize = [800, 600]
+        state.gamemode = "play"
+
+        mock_focus_object = MagicMock()
+        mock_focus_object.pos = [1000, 800]
+        mock_focus_object.parallax = 2  # Parallax value
+
+        camera = cam()
+        camera.focusobj = mock_focus_object
+        camera.update()
+
+        # Depth and position should reflect parallax
+        self.assertEqual(camera.depth, 1)
+        self.assertEqual(camera.pos, (600, 500))  # Adjusted for parallax
     
+    def test_update_in_edit_mode(self):
+        state.screensize = [800, 600]
+        state.gamemode = "edit"
+        state.objects = []
+
+        camera = cam()
+        camera.focus = [100, 100]
+        camera.update()
+
+        # Camera should freely move in edit mode
+        self.assertEqual(camera.pos, (-300, -200))  # Centering focus
 
 unittest.main()
